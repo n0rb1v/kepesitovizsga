5aa5bf5173877099a2eabcf98ab1530889684e31
n0rb1v <n0rb1v@gmail.com>
2021-04-29 12:47:26 +0200

    1

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -0,0 +1,16 @@
+package hu.nive.ujratervezes.kepesitovizsga.addigits;
+
+public class AddDigits {
+    public int addDigits(String input) {
+        if (input == null || input.isEmpty()) {
+            return -1;
+        }
+        int sum = 0;
+        for (char item : input.toCharArray()) {
+            if (Character.isDigit(item)) {
+                sum += Integer.parseInt(String.valueOf(item));
+            }
+        }
+        return sum;
+    }
+}
